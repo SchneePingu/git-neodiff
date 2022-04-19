4d19ee8b4e3e3b4e2abd1ecedf006085913f2f9c
SchneePingu <>
2022-04-19 22:23:51 +0200

    handle redirect with curl

--- install.sh
+++ install.sh
@@ -10,22 +10,22 @@ fi
 cd "$INSTALL_DIR"
 
 NEODIFF_URL="https://github.com/SchneePingu/git-neodiff/releases/download/$RELEASE_TAG/neodiff"
-if curl --output /dev/null --silent --head --fail "$NEODIFF_URL"; then
-  if curl --silent --fail -O "$NEODIFF_URL"; then
+if curl --output /dev/null --silent --head --fail -L "$NEODIFF_URL"; then
+  if curl --silent --fail -L -O "$NEODIFF_URL"; then
     chmod +x "neodiff"
   fi
 fi
 
 GIT_NEODIFF_URL="https://github.com/SchneePingu/git-neodiff/releases/download/$RELEASE_TAG/git-neodiff"
 if curl --output /dev/null --silent --head --fail "$GIT_NEODIFF_URL"; then
-  if curl --silent --fail -O "$GIT_NEODIFF_URL"; then
+  if curl --silent --fail -L -O "$GIT_NEODIFF_URL"; then
     chmod +x "git-neodiff"
   fi
 fi
 
 GIT_NEOSHOW_URL="https://github.com/SchneePingu/git-neodiff/releases/download/$RELEASE_TAG/git-neoshow"
 if curl --output /dev/null --silent --head --fail "$GIT_NEOSHOW_URL"; then
-  if curl --silent --fail -O "$GIT_NEOSHOW_URL"; then
+  if curl --silent --fail -L -O "$GIT_NEOSHOW_URL"; then
     chmod +x "git-neoshow"
   fi
 fi
