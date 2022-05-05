b7c4277919219366202d12f436bf265eda6f0549
SchneePingu <>
2022-05-05 22:23:53 +0200

    test showing added, deleted and unchanged lines

--- test/viewerTests/TestLineChangesViewer.hs
+++ test/viewerTests/TestLineChangesViewer.hs
@@ -4,6 +4,7 @@ import Test.HUnit
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import LineChangesViewer
+import LineChangeData
 import Text
 
 
@@ -11,9 +12,59 @@ unitTests = testGroup "TestLineChangesViewer" testCases
 
 testCases =
     [ testNoTextForNoLineChangesEmpty
+    , testTextForAddedLine
+    , testTextForDeletedLine
+    , testTextForUnchangedLine
     ]
 
 testNoTextForNoLineChangesEmpty =
     testCase "No text is shown for no line changes."
     $ viewLineChanges [[]]
     @?= []
+
+
+testTextForAddedLine =
+    testCase "Text is shown for added line."
+    $ viewLineChanges [[LineChange AddedLine (0, 1) "added"]]
+    @?= expectedText
+    where
+      expectedText =
+        color green "+" ++
+        space ++
+        view "" ++
+        view "" ++
+        color yellow "1:" ++
+        space ++
+        color green "added" ++
+        newline
+
+testTextForDeletedLine =
+    testCase "Text is shown for deleted line."
+    $ viewLineChanges [[LineChange DeletedLine (1, 0) "deleted"]]
+    @?= expectedText
+    where
+      expectedText =
+        color red "-" ++
+        space ++
+        view "1:" ++
+        view "" ++
+        color yellow "" ++
+        space ++
+        color red "deleted" ++
+        newline
+
+
+testTextForUnchangedLine =
+    testCase "Text is shown for unchanged line."
+    $ viewLineChanges [[LineChange UnchangedLine (1, 2) "unchanged"]]
+    @?= expectedText
+    where
+      expectedText =
+        view " " ++
+        space ++
+        view "1:" ++
+        view " " ++
+        color yellow "2:" ++
+        space ++
+        view "unchanged" ++
+        newline
