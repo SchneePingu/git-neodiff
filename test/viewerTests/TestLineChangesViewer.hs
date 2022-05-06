2ceceb7921111aeae8327ef85bf57d955200ac50
SchneePingu <>
2022-05-06 22:36:20 +0200

    add tests for LineChangesViewer

--- test/viewerTests/TestLineChangesViewer.hs
+++ test/viewerTests/TestLineChangesViewer.hs
@@ -15,7 +15,12 @@ testCases =
     , testTextForAddedLine
     , testTextForDeletedLine
     , testTextForUnchangedLine
+    , testTextForSectionOfLineChanges
+    , testTextForMultipleSectionsOfLineChanges
+    , testTextForPaddingOfLineNumbers
     ]
+
+
 
 testNoTextForNoLineChangesEmpty =
     testCase "No text is shown for no line changes."
@@ -25,77 +30,124 @@ testNoTextForNoLineChangesEmpty =
 
 testTextForAddedLine =
     testCase "Text is shown for added line."
-    $ viewLineChanges [[LineChange AddedLine (0, 1) "added"]]
-    @?= expectedText
-    where
-      expectedText =
-        color green "+" ++
-        space ++
-        view "" ++
-        view "" ++
-        color yellow "1:" ++
-        space ++
-        color green "added" ++
-        newline
+    $ viewLineChanges [[addedLine]]
+    @?= textForAddedLine
 
 testTextForDeletedLine =
     testCase "Text is shown for deleted line."
-    $ viewLineChanges [[LineChange DeletedLine (1, 0) "deleted"]]
-    @?= expectedText
-    where
-      expectedText =
-        color red "-" ++
-        space ++
-        view "1:" ++
-        view "" ++
-        color yellow "" ++
-        space ++
-        color red "deleted" ++
-        newline
+    $ viewLineChanges [[deletedLine]]
+    @?= textForDeletedLine
 
 
 testTextForUnchangedLine =
     testCase "Text is shown for unchanged line."
-    $ viewLineChanges [[LineChange UnchangedLine (1, 2) "unchanged"]]
+    $ viewLineChanges [[unchangedLine]]
+    @?= textForUnchangedLine
+
+testTextForSectionOfLineChanges =
+    testCase "Text is shown for section of line changes."
+    $ viewLineChanges [sectionOfLineChanges]
+    @?= textForSectionOfLineChanges
+
+testTextForMultipleSectionsOfLineChanges =
+    testCase "Text is shown for multiple sections of line changes."
+    $ viewLineChanges [sectionOfLineChanges, sectionOfLineChanges]
+    @?= expectedText
+    where
+      expectedText =
+        textForSectionOfLineChanges ++
+        newline ++
+        textForSectionOfLineChanges
+
+testTextForPaddingOfLineNumbers =
+    testCase "Text is shown with padding for line numbers."
+    $ viewLineChanges [[ unchangedLineWithLineNumbers (3, 4)
+                       , unchangedLineWithLineNumbers (13, 14) ]]
     @?= expectedText
     where
       expectedText =
         view " " ++
         space ++
-        view "1:" ++
+        view " 3:" ++
         view " " ++
-        color yellow "2:" ++
+        color yellow " 4:" ++
+        space ++
+        view "unchanged" ++
+        newline ++
+        view " " ++
+        space ++
+        view "13:" ++
+        view " " ++
+        color yellow "14:" ++
         space ++
         view "unchanged" ++
         newline
 
-testTextForMultipleLineChanges =
-    testCase "Text is shown for unchanged line."
-    $ viewLineChanges [[LineChange UnchangedLine (1, 2) "unchanged", LineChange DeletedLine (3, 0) "deleted", LineChange AddedLine (0, 4) "added"]]
-    @?= expectedText
-    where
-      expectedText =
-        view " " ++
-        space ++
-        view "1:" ++
-        view " " ++
-        color yellow "2:" ++
-        space ++
-        view "unchanged" ++
-        newline ++
-        color red "-" ++
-        space ++
-        view "3:" ++
-        view " " ++
-        color yellow "  " ++
-        space ++
-        color red "deleted" ++
-        newline ++
-        color green "+" ++
-        space ++
-        view "  " ++
-        view " " ++
-        color yellow "4:" ++
-        space ++
-        color green "added" ++
-        newline
+addedLine = LineChange AddedLine (0, 1) "added"
+
+textForAddedLine =
+  color green "+" ++
+  space ++
+  view "" ++
+  view "" ++
+  color yellow "1:" ++
+  space ++
+  color green "added" ++
+  newline
+
+deletedLine = LineChange DeletedLine (2, 0) "deleted"
+
+textForDeletedLine =
+  color red "-" ++
+  space ++
+  view "2:" ++
+  view "" ++
+  color yellow "" ++
+  space ++
+  color red "deleted" ++
+  newline
+
+unchangedLine = LineChange UnchangedLine (3, 4) "unchanged"
+
+textForUnchangedLine =
+  view " " ++
+  space ++
+  view "3:" ++
+  view " " ++
+  color yellow "4:" ++
+  space ++
+  view "unchanged" ++
+  newline
+
+sectionOfLineChanges = [addedLine, deletedLine, unchangedLine]
+
+textForSectionOfLineChanges =
+  color green "+" ++
+  space ++
+  view "  " ++
+  view " " ++
+  color yellow "1:" ++
+  space ++
+  color green "added" ++
+  newline ++
+  color red "-" ++
+  space ++
+  view "2:" ++
+  view " " ++
+  color yellow "  " ++
+  space ++
+  color red "deleted" ++
+  newline ++
+  view " " ++
+  space ++
+  view "3:" ++
+  view " " ++
+  color yellow "4:" ++
+  space ++
+  view "unchanged" ++
+  newline
+
+
+unchangedLineWithLineNumbers :: LineNumbers -> LineChange
+unchangedLineWithLineNumbers lineNumbers =
+  LineChange UnchangedLine lineNumbers "unchanged"
